6b6877e6f56e6c420fdec4a3a2746ebe89c717e5
gorod0k <68774622+gorod0k@users.noreply.github.com>
2026-02-11 16:08:40 +0000

    debug

--- src/java/com/local/Server.java
+++ src/java/com/local/Server.java
@@ -20,19 +20,17 @@ public class Server {
 
         // Используем Handler, чтобы точно показывать Toasts в UI-потоке
         Handler uiHandler = new Handler(Looper.getMainLooper());
-        final File logFile;
+        File log;
         try {
-            // Для Android 10+ используем getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)
             java.io.File downloadsDir = context.getExternalFilesDir(android.os.Environment.DIRECTORY_DOWNLOADS);
             if (downloadsDir == null) {
-                // fallback для старых устройств
                 downloadsDir = android.os.Environment.getExternalStoragePublicDirectory(android.os.Environment.DIRECTORY_DOWNLOADS);
             }
-            logFile = new File(downloadsDir, "php_server_log.txt");
+            log = new File(downloadsDir, "php_server_log.txt");
         } catch (Exception e) {
-            // fallback на внутреннюю память, если что-то пошло не так
-            logFile = new File(context.getCacheDir(), "php_server_log.txt");
+            log = new File(context.getCacheDir(), "php_server_log.txt");
         }
+        final File logFile = log;
 
         try {
             // 1. Ищем бинарник
